cf61cb2fc18ed856551bc9d3d6cefa31e8f2cd6d
日瀬 健人 <kentnewbie@hotmail.com>
2021-07-30 21:56:40 +0900

    T

---
+++ ElibrarTest/Backup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Aritiafel.Characters.Heroes;
+
+namespace ElibrarTest
+{
+    [TestClass]
+    public class Backup
+    {
+        [TestMethod]
+        public void BackupProject()
+        {
+            Tina.SaveProject("WinForm", "Elibrar");
+        }
+    }
+}
---
+++ ElibrarTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ElibrarTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ElibrarTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d3c1fde9-9a3e-4342-a747-95283b3d0c1b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
